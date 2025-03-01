c7f6d209c86b868014b2561eef4025bfe5af6a04
Vaibhavi shah <vaibhavi.shah2804@gmail.com>
2025-03-01 12:58:29 -0500

    " Extension pop up created according to the new layout"

--- chromeext_frontend/popup.js
+++ chromeext_frontend/popup.js
@@ -1,13 +1,35 @@
 const button = document.getElementById("b");
+const cancelButton = document.getElementById("cancel_btn");
+const nameDisplay = document.getElementById("nameDisplay");
+const llmSelect = document.getElementById("llmSelect");
+const tokenSelect = document.getElementById("tokenSelect");
+const llmKey = document.getElementById("llmKey");
+
 const summary = document.getElementById("summary");
 const intent = document.getElementById("intent");
 const impact = document.getElementById("impact");
 const instruction = document.getElementById("instruction");
 
-window.addEventListener("DOMContentLoaded", () => 
-{
-    button.addEventListener('click', () => 
-        {
+window.addEventListener("DOMContentLoaded", () => {
+
+    button.addEventListener("click", () => {
         chrome.runtime.sendMessage({ action: "buttonClicked" });
     });
-})
+
+    cancelButton.addEventListener("click", () => {
+        console.log("Cancelled");
+        nameDisplay.innerText = "Action Cancelled";
+    });
+
+    llmSelect.addEventListener("change", () => {
+        console.log("Selected LLM:", llmSelect.value);
+    });
+
+    tokenSelect.addEventListener("change", () => {
+        console.log("Selected Token:", tokenSelect.value);
+    });
+
+    llmSelect.addEventListener("change", () => {
+        llmKey.value = llmSelect.value === "openai" ? "OpenAI-API-KEY" : "Gemini-API-KEY";
+    });
+});
