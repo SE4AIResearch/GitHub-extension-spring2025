77f2fb2e00321e5065c6c1dc8220c54b15720393
Ishan Garg <ishan.garg10394@gmail.com>
2025-03-02 06:42:22 -0500

    SCRUM-6: Endpoints for updating LLM key and Github key

--- chromeext_backend/src/main/java/saim/ApiKeyRepo.java
+++ chromeext_backend/src/main/java/saim/ApiKeyRepo.java
@@ -5,7 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface ApiKeyRepo extends JpaRepository<ApiKey, Long>{
 
-
+    Optional<ApiKey> findByUuid(String uuid);
 }
