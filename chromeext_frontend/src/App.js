83b781890636215efffed39824ff5c7e6401cc25
Vaibhavi shah <vaibhavi.shah2804@gmail.com>
2025-04-09 20:17:16 -0400

    "chart Filter created"

--- chromeext_frontend/src/App.js
+++ chromeext_frontend/src/App.js
@@ -5,12 +5,12 @@ import Dashboard from "./Dashboard.js";
 function App() {
   console.log("Inside App js");
   return (
-   <Dashboard />
-   /* <Router>
+   //<Dashboard />
+    <Router>
       <Routes>
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
-    </Router> */
+    </Router> 
   );
 }
 
